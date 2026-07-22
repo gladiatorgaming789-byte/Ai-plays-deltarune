756ebbdf5d07fda04cc5fbd1e962c7ab9c924d52
GladiatorGaming <gladiatorgaming789@gmail.com>
2026-07-22 11:04:14 -0500

    Route GUI through safe profile launcher

--- deltarune_agent/__main__.py
+++ deltarune_agent/__main__.py
@@ -1,5 +1,11 @@
-from .runner import main
+import sys
 
-if __name__ == "__main__":
+
+if len(sys.argv) >= 2 and sys.argv[1] == "gui":
+    from .profile_launcher import launch_profile_launcher
+
+    launch_profile_launcher()
+else:
+    from .runner import main
+
     main()
-
