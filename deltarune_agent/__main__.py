5e0b44dd91efa0bf2f5892d04d0094fa1dca7e8f
GladiatorGaming <gladiatorgaming789@gmail.com>
2026-07-26 18:49:06 -0500

    Use hardened runtime entrypoint

--- deltarune_agent/__main__.py
+++ deltarune_agent/__main__.py
@@ -6,6 +6,24 @@ if len(sys.argv) >= 2 and sys.argv[1] == "gui":
 
     launch_integrated_gui()
 else:
-    from .runner import main
+    from .replay import print_replay, replay_run
+    from .run19_runner import run
+    from .runner import build_parser, listen
 
-    main()
+    args = build_parser().parse_args()
+    if args.command == "run":
+        run(args)
+    elif args.command == "telemetry":
+        listen(args)
+    elif args.command == "replay":
+        print_replay(
+            replay_run(
+                args.run_directory,
+                args.visual_memory,
+                save_report=not args.no_save,
+            )
+        )
+    elif args.command == "gui":
+        from .integrated_gui import launch_integrated_gui
+
+        launch_integrated_gui()
